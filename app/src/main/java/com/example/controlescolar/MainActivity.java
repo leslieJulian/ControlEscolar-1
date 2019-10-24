49a612c0fe5865474e225a4160af5f1d07e41117
Gamaliel De los santos <gama.amc@gmail.com>
2019-10-24 13:32:19 -0500

    Interfaz registrar estudiante

--- app/src/main/java/com/example/controlescolar/MainActivity.java
+++ app/src/main/java/com/example/controlescolar/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.controlescolar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+        btnRegistrar.setOnClickListener(registrar);
     }
+
+    //EVENTOS PARA ABRIR OTRA ACTIVITY
+    View.OnClickListener registrar = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), RegistrarEstudiante.class);
+            intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+    };
 }
