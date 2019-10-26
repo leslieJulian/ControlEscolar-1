aa46499485ce963be9d70d21363d11fa7fe3c207
Gamaliel De los santos <gama.amc@gmail.com>
2019-10-26 12:20:12 -0500

    Conexión a FireBase

--- app/src/main/java/com/example/controlescolar/MainActivity.java
+++ app/src/main/java/com/example/controlescolar/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.controlescolar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Prueba para cambiar el texto por el nombre del instituto
+
+        FirebaseDatabase.getInstance().getReference().child("datos").child("520")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()){
+                            Tecnologico datosTec = dataSnapshot.getValue(Tecnologico.class);
+                            TextView textView = findViewById(R.id.tvPrincipal);
+                            textView.setText(datosTec.getNombre());
+                        }else{
+                            Toast.makeText(getApplicationContext(), "No existe el instituto con clave 520", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 }
