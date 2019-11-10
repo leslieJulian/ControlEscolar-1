e902851dd59f3fa63040b1e7bce44575093441de
AbrahamPerezNava <beny.perez.sb6@gmail.com>
2019-11-10 15:30:56 -0600

    c

--- app/src/main/java/com/example/controlescolar/FragmentoEstudiantes.java
+++ app/src/main/java/com/example/controlescolar/FragmentoEstudiantes.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -21,8 +22,11 @@ public class FragmentoEstudiantes extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_visualizar_estudiante, container, false);
+        View v = inflater.inflate(R.layout.fragment_estudiantes, container, false);
 
+        Button registrar = v.findViewById(R.id.btnRegistrar);
+
+        Button visualizar = v.findViewById(R.id.btnVisualizar);
 
         return v;
     }
