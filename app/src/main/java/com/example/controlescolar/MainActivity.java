de89fd4ef27f694f5cb777243efd79dbe6ec3fb0
Hector-Lab <56307732+Hector-Lab@users.noreply.github.com>
2019-11-13 10:45:30 -0600

    CRUD Grupos y ocupaciones, **Error al modificar**

--- app/src/main/java/com/example/controlescolar/MainActivity.java
+++ app/src/main/java/com/example/controlescolar/MainActivity.java
@@ -3,40 +3,42 @@ package com.example.controlescolar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
+import Controller.ControllerAula;
+import POJO.aulas;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Prueba para cambiar el texto por el nombre del instituto
-
-        FirebaseDatabase.getInstance().getReference().child("datos").child("520")
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if(dataSnapshot.exists()){
-                            Tecnologico datosTec = dataSnapshot.getValue(Tecnologico.class);
-                            TextView textView = findViewById(R.id.tvPrincipal);
-                            textView.setText(datosTec.getNombre());
-                        }else{
-                            Toast.makeText(getApplicationContext(), "No existe el instituto con clave 520", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
     }
+    public void grupos(View v){
+        Intent intent = new Intent(this,ListaGrupos.class);
+        startActivity(intent);
+
+    }
+    public void agregar(View V){
+        Intent intent = new Intent(this,grupoActivity.class);
+        intent.putExtra("Tipo","Agregar");
+        startActivity(intent);
+    }
+
 }
